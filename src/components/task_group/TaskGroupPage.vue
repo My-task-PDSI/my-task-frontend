9f5c7d23bafc0b504d61cec75c7c06bf8806f01a
FranciscoCharles <charlesdcf123@gmail.com>
2021-12-30 11:30:49 -0300

    add TaskGroupPage

---
+++ src/components/task_group/TaskGroupPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{$options.name}}</h1>
+  <TaskGroup id-group="1"/>
+</template>
+
+<script>
+import TaskGroup from './TaskGroup.vue';
+export default {
+    name:'TaskGroupPage',
+    
+    components:{
+        TaskGroup
+    }
+}
+</script>
+
+<style>
+
+</style>
