8bbb8d98c57989d1770132cc1d712c23bdbf4f27
FranciscoCharles <charlesdcf123@gmail.com>
2021-12-30 18:33:26 -0300

    update codes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,34 +1,32 @@
 <template>
-    <div class="nav">
-        <router-link class="navitem" to="/#">Home</router-link>
+  <div class="nav">
+    <router-link class="navitem" to="/#">Home</router-link>
 
-        <router-link class="navitem" to="/about">About</router-link>
-        <router-link class="navitem" to="/contact">Contact Us</router-link>
-        <router-link class="navitem" to="/task-group">Task group</router-link>
+    <router-link class="navitem" to="/about">About</router-link>
+    <router-link class="navitem" to="/contact">Contact Us</router-link>
+    <router-link class="navitem" to="/task-group">Task group</router-link>
 
-        <div class="signin">
-            <router-link class="navitem" to="/login">Login</router-link>
-            <router-link class="navitem" to="/signup">SignUp</router-link>
-        </div>
+    <div class="signin">
+      <router-link class="navitem" to="/login">Login</router-link>
+      <router-link class="navitem" to="/signup">SignUp</router-link>
     </div>
+  </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
 <style scoped>
-    .nav {
-        display: flex;
-    }
-    .navitem {
-        margin-left: 20px;
-    }
-    .signin {
-        margin-left: 70%;
-    }
-    .signin .navitem {
-        margin: 10px;
-    }
+.nav {
+  display: flex;
+  width: 100%;
+  border: 2px solid red;
+}
+.navitem {
+  margin-left: 20px;
+}
+.signin .navitem {
+  margin: 10px;
+}
 </style>
