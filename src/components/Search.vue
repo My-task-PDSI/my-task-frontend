2e3e26e7438605377147ded91fce43803c025afd
FranciscoCharles <charlesdcf123@gmail.com>
2022-01-02 11:41:58 -0300

    create new components [Search, TheTitleAndDate]

---
+++ src/components/Search.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="search-container">
+    <input
+      @search="onSearch"
+      type="search"
+      name="search"
+      placeholder="Pesquise por tarefas..."
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  emits: ["search"],
+  methods: {
+    onSearch(event) {
+      this.$emit("search", event.target.value);
+    },
+  },
+};
+</script>
+
+<style>
+.search-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 1;
+
+  height: 100%;
+  padding: 10px 0;
+}
+.search-container input {
+  height: 40px;
+  width: 90%;
+  border-radius: 10px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  background: transparent;
+  font-size: 16px;
+  padding: 0 10px;
+}
+</style>
---
+++ src/components/TheTitleAndDate.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="header">
+    <h1>{{ title }}</h1>
+    <h3>{{ currentDay }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheTitleAndDate",
+  props: {
+    title: {
+      type: String,
+      default: "Inicio",
+    },
+  },
+  data() {
+    return {
+      currentDay: new Date().toLocaleDateString("pt-BR", {
+        month: "long",
+        day: "numeric",
+      }),
+    };
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  width: 210px;
+  height: auto;
+  margin-left: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.header > h1 {
+  color: black;
+  font-size: 24px;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+</style>
