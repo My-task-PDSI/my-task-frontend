e29b67973e77337b029c0e4eef7d0b704c238ae6
FranciscoCharles <charlesdcf123@gmail.com>
2022-01-07 12:22:27 -0300

    alterando rota task-groups

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,21 @@
-import { createWebHistory, createRouter } from "vue-router";
-import Login from "../components/signin/Login.vue";
-import SignUp from "../components/signin/SignUp.vue";
-import HomePage from "../components/landing/HomePage.vue"
-import About from "../components/landing/About.vue"
-import Contact from "../components/landing/Contact.vue"
-import TaskGroupPage from "../components/task_group/TaskGroupPage.vue"
-import TaskGroup from "../components/task_group/TaskGroup.vue"
+import { createWebHistory, createRouter } from 'vue-router';
+import Login from '../components/signin/Login.vue';
+import SignUp from '../components/signin/SignUp.vue';
+import HomePage from '../components/landing/HomePage.vue'
+import About from '../components/landing/About.vue'
+import Contact from '../components/landing/Contact.vue'
+import TaskGroupPage from '../components/task_group/TaskGroupPage.vue'
+import TaskGroup from '../components/task_group/TaskGroup.vue'
 
 const routes = [
 
-  { path: "/", component: HomePage },
-  { path: "/login", component: Login },
-  { path: "/signup", component: SignUp },
-  { path: "/about", component: About },
-  { path: "/contact", component: Contact },
-  {path: "/task-groups", component:TaskGroupPage },
-  { path: "/task-groups/tasks/", component: TaskGroup , name:'group'},
+  { path: '/', component: HomePage },
+  { path: '/login', component: Login },
+  { path: '/signup', component: SignUp },
+  { path: '/about', component: About },
+  { path: '/contact', component: Contact },
+  {path: '/task-groups', name:'task-groups', component:TaskGroupPage },
+  { path: '/task-groups/tasks/', component: TaskGroup , name:'group'},
 ];
 
 const router = createRouter({
