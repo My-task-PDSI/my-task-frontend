6e90289a70d6949ea071eb229338c6fd7d992e69
FranciscoCharles <charlesdcf123@gmail.com>
2022-01-12 19:40:52 -0300

    criando componentes de membros

---
+++ src/components/member/Member.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="member-container">
+    <div class="img-container no-user-select">
+    <h3>{{username[0]}}</h3>
+    </div>
+    <h3>{{email}}</h3>
+    <h3>convidado</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Member",
+  props:{
+    username:String,
+    email:String,
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<style scoped>
+.member-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: auto;
+  min-width: 50px;
+  margin:5px;
+}
+.img-container{
+  height: 30px;
+  width: 30px;
+  display: flex;
+  justify-content:center;
+  align-items: center;
+  border-radius: 50%;
+  overflow: hidden;
+  background-color:rgb(0, 174, 255);
+}
+.img-container > h3{
+  font-weight: bolder;
+  color:white;
+}
+.member-container > h3{
+  height: 30px;
+  margin-left:10px;
+}
+</style>
---
+++ src/components/member/MemberList.vue
@@ -0,0 +1,69 @@
+<template>
+ <div class="member-list-container">
+    <div class="controles">
+    <div class="title">
+      <h3>Membros</h3>
+    </div>
+    <div class="eye-container no-user-select" @click="toogleVisibilty">
+      <i v-if="visible" class="fas fa-eye"></i>
+      <i v-else class="fas fa-eye-slash"></i>
+    </div>
+    </div>
+    <div v-if="visible" class="member-list">
+    <Member
+      v-for="item in memberList"
+      :key="item"
+      :username="'teste'"
+      :email="'teste@gmail.com'"
+    />
+    </div>
+  </div>
+</template>
+
+<script>
+import Member from "./Member.vue";
+export default {
+  name: "MemberList",
+  components:{
+    Member
+  },
+  props:{
+    username:String,
+    email:String,
+    memberList:Array
+  },
+  data(){
+    return {
+      visible:false,
+      };
+  },
+  methods: {
+    toogleVisibilty(){
+this.visible = !this.visible;
+    }
+  },
+};
+</script>
+
+<style scoped>
+.member-list-container {
+  width: 100%;
+  display: flex;
+  flex-direction:column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  margin-bottom: 10px;
+}
+.member-list{
+  padding-left: 15px;
+}
+.controles{
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap:10px;
+}
+.eye-container{
+  font-size: 24px;
+}
+</style>
