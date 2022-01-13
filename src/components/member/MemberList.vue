459b8c1f2c3d5d9d64e8eb3473d3cfc4695cc53a
leocristian <leonardosclopes@gmail.com>
2022-01-13 15:21:20 -0300

    add get all users on memberList

--- src/components/member/MemberList.vue
+++ src/components/member/MemberList.vue
@@ -5,11 +5,14 @@
       <h3>Membros</h3>
     </div>
     <div class="eye-container no-user-select" @click="toogleVisibilty">
-      <i v-if="visible" class="fas fa-eye"></i>
+      <i v-if="membersGroupVisible" class="fas fa-eye"></i>
       <i v-else class="fas fa-eye-slash"></i>
     </div>
+    <div class="addMemberBtn">
+      <button @click="addMemberModal">Add Member</button>
     </div>
-    <div v-if="visible" class="member-list">
+    </div>
+    <div v-if="membersGroupVisible" class="member-list">
     <Member
       v-for="item in memberList"
       :key="item"
@@ -17,29 +20,48 @@
       :email="'teste@gmail.com'"
     />
     </div>
+    <div v-if="membersAvailableVisible" class="member-list">
+      <MembersAvailable
+      v-for="item in memberList"
+      :key="item"
+      :username="item.username"
+      :email="item.email"
+    />
+    </div>
   </div>
 </template>
 
 <script>
+import Api from '../../services/api';
 import Member from "./Member.vue";
+import MembersAvailable from "./MermbersAvailable.vue"
 export default {
   name: "MemberList",
   components:{
-    Member
+    Member,
+    MembersAvailable
   },
   props:{
     username:String,
     email:String,
-    memberList:Array
+    memberList:Array,
+    membersAvailableList:Array
   },
   data(){
     return {
-      visible:false,
+      membersGroupVisible:false,
+      membersAvailableVisible: false
       };
   },
   methods: {
     toogleVisibilty(){
-this.visible = !this.visible;
+      this.membersGroupVisible = !this.membersGroupVisible;
+    },
+    async addMemberModal(){
+        const response = await Api.get("user/all/1");
+        console.log(response.data.allUsers)
+        this.$memberList = response.data.allUsers
+        this.membersAvailableVisible = !this.membersAvailableVisible;
     }
   },
 };
